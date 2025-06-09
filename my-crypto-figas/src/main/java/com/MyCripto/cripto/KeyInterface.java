5fe1f7d107aa537f4eed33950f1a4e3b26c3a200
rafael <rafael.19.rosendo@gmail.com>
2025-06-08 21:06:08 -0300

    AES interface

---
+++ my-crypto-figas/src/main/java/com/MyCripto/cripto/KeyInterface.java
@@ -0,0 +1,26 @@
+package main.java.com.MyCripto.cripto;
+
+
+/*
+ * So this interface gonna have the key methods 
+ * 
+ */
+
+public interface KeyInterface {
+
+    //Assic get methods
+    public List<List<Integer>> getAssic(List<String> seed);
+    public Integer getAssicKey(String passworld);
+
+    // Operations assic methods
+    public List<List<Integer>> addAssic(String passworld);
+    public List<List<Integer>> subAssic(String passworld);
+    public List<List<Integer>> multiAssic(String passworld);
+
+    public List<List<Integer>> RevaddAssic(String passworld);
+    public List<List<Integer>> RevsubAssic(String passworld);
+    public List<List<Integer>> RevmultiAssic(String passworld); 
+
+    // AES 
+
+}
